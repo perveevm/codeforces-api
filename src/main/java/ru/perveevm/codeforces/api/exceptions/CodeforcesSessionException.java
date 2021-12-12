e99c944f1f7852254efa556bd40387c71a1c5856
perveevm <perveev_m@mail.ru>
2021-12-13 01:27:01 +0300

    apiSig and apiKey are removed from exceptions' messages for security

--- src/main/java/ru/perveevm/codeforces/api/exceptions/CodeforcesSessionException.java
+++ src/main/java/ru/perveevm/codeforces/api/exceptions/CodeforcesSessionException.java
@@ -1,6 +1,7 @@
 package ru.perveevm.codeforces.api.exceptions;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +21,13 @@ public class CodeforcesSessionException extends Exception {
     protected static String getMessage(final String url, final List<NameValuePair> parameters) {
         return String.format("Error happened while performing POST request to %s with parameters %s", url,
                 parameters.stream()
+                        .map(param -> {
+                            if (param.getName().equals("apiKey") || param.getName().equals("apiSig")) {
+                                return new BasicNameValuePair(param.getName(), "Hidden for security");
+                            } else {
+                                return param;
+                            }
+                        })
                         .map(param -> String.format("\"%s\": \"%s\"", param.getName(), param.getValue()))
                         .collect(Collectors.joining(", ", "{", "}")));
     }
