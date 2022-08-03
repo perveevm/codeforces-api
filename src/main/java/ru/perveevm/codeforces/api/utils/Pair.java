51dcff205e24a0f9f8f7f308d72556cf65dce6fa
Perveev Mike <perveev_m@mail.ru>
2022-08-04 00:47:26 +0300

    Pair::toString added

--- src/main/java/ru/perveevm/codeforces/api/utils/Pair.java
+++ src/main/java/ru/perveevm/codeforces/api/utils/Pair.java
@@ -33,4 +33,9 @@ public class Pair<T1, T2> {
     public T2 getSecond() {
         return second;
     }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
 }
