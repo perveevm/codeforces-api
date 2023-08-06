9aa70a5f82e9993bbc44017404d7148aea7968d4
Mike <perveev_m@mail.ru>
2023-08-06 16:47:00 +0300

    Equals and hashCode

--- src/main/java/ru/perveevm/codeforces/api/utils/Pair.java
+++ src/main/java/ru/perveevm/codeforces/api/utils/Pair.java
@@ -1,5 +1,7 @@
 package ru.perveevm.codeforces.api.utils;
 
+import java.util.Objects;
+
 /**
  * @author Perveev Mike (perveev_m@mail.ru)
  * <p>
@@ -37,5 +39,23 @@ public class Pair<T1, T2> {
     @Override
     public String toString() {
         return "(" + first + ", " + second + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (!Objects.equals(first, pair.first)) return false;
+        return Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
     }
 }
